3e8549dd59e6ad0bf62fc5ac564dc49ed0bd2b97
rotil <42150485+rotil@users.noreply.github.com>
2023-11-04 10:49:34 +0100

    my first commit

---
+++ api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT_NUMBER = 80
+
+mappings = { '/hello': {'msg': 'Hello World, my people'}, '/health': {'msg': 'Healthy'} }
+
+class myHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		status = 200
+
+		service = mappings.get(self.path)
+		if service is None:
+			status = 404	
+			service = {'msg': 'Not found'}			
+
+		self.send_response(status)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write(bytes(service['msg'], 'ASCII'))
+		return
+
+try:
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print ('Started httpserver on port ' , PORT_NUMBER)
+	
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print ('^C received, shutting down the web server')
+	server.socket.close()
+	
